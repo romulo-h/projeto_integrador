10e63727f53cfdbac2f18472649645e3699e5fee
romulo-h <romulo.h.oliveira@gmail.com>
2021-03-21 16:20:24 -0300

    banco

---
+++ ProjIntegrador/Models/Doacoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjIntegrador.Models
+{
+    public class Doacoes
+    {
+        [Key]
+        public int IdDoacao { get; set; }
+        public int Blusa { get; set; }
+        public int Jaqueta { get; set; }
+    }
+}
